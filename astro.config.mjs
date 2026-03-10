b97bff739d61005da0860298d88ef1829795a523
Kamden Benbow <kbniemier@gmail.com>
2026-03-10 18:56:45 -0500

    Add Mailchimp newsletter signup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel(),
   redirects: {
     '/blog/italy': '/blog/italy-7-day-itinerary-rome-florence-sorrento',
   },
