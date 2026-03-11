7d30c54d383ec9520a1249aeb2e2b1f2c0af9143
Kamden Benbow <kbniemier@gmail.com>
2026-03-10 20:35:44 -0500

    Switch to client-side Mailchimp JSONP, remove Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
   redirects: {
     '/blog/italy': '/blog/italy-7-day-itinerary-rome-florence-sorrento',
   },
