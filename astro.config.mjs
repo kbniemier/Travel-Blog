395d968b70f4aaa5c7c42de61aefb6e9ec382657
Kamden Benbow <kbniemier@gmail.com>
2026-03-10 19:11:27 -0500

    Fix: use output static from Astro 5 compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   redirects: {
     '/blog/italy': '/blog/italy-7-day-itinerary-rome-florence-sorrento',
