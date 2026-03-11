7388e02b53dce0fa4140a35fa3c290afbb177455
Kamden Benbow <kbniemier@gmail.com>
2026-03-10 20:03:31 -0500

    Switch to output server to fix API route POST handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   redirects: {
     '/blog/italy': '/blog/italy-7-day-itinerary-rome-florence-sorrento',
